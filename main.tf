add661c4ce7ccdcb34292372248b9ce670cdc761
furquan123 <124357625+furquan123@users.noreply.github.com>
2023-09-09 23:30:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ name = each.value
 variable "user_names" {
 description = "*"
 type = set(string)
-default = ["user1", "user2", "user3", "user4"]
+default = ["usera", "userb", "userc", "userd"]
 }
 
 resource "aws_ebs_volume" "eight" {
