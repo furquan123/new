34c8347cd368fecd60506dec66b9eff740cb7cd3
furkhan <124357625+furquan123@users.noreply.github.com>
2023-10-17 14:38:21 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "fur-elb"
-  availability_zones = ["ap-south-1b", "ap-south-1c"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
     instance_port     = 80
